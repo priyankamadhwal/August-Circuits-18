e67e1463a9451490e1e2af74be13a8aeb942b275
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-08-19 21:36:01 +0530

    Create Q2- Picu Bank.cpp

---
+++ Q2- Picu Bank.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    long long T, D, A, M, B, X, ans;
+    
+    cin>>T;
+    
+    for (int test=1; test<=T; test++)
+    {
+        cin>>D>>A>>M>>B>>X;
+        
+        //Unitary Method
+        /*
+        In (M+1) months --> A*M + B dollars
+        1 dollar --> (M+1) / (A*M + B)
+        X-D dollar --> (X-D)(M+1) / (A*M + B)
+        
+        */
+        
+        if (D>X)
+            ans=0;
+        else 
+          ans=round(float((X-D)*(M+1)) / (A*M + B));
+        cout<<ans<<"\n";
+        
+        //TLE ERROR
+        /*ans=0;
+        while (D<X)
+        {
+            if (D+M*A+B>X)
+                break;
+            else
+            {
+                D+=M*A+B;
+                ans+=M+1;
+            }
+        }
+        for (int i=1; i<=M+1 && D<X; i++)
+        {
+            if (i==M+1)
+                D+=B;
+            else
+                D+=A;
+            ans++;
+        }
+        cout<<ans<<"\n";*/
+    }
+    
+    return 0;
+}
