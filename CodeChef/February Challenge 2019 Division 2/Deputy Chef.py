ef56240489b87a12f2b709763f0d189116f13081
Priyanka Madhwal <priyanka0701m@gmail.com>
2019-02-11 13:46:42 +0530

    DEPCHEF

--- CodeChef/February Challenge 2019 Division 2/Deputy Chef.py
+++ CodeChef/February Challenge 2019 Division 2/Deputy Chef.py
@@ -2,15 +2,15 @@
 
 for _ in range(int(input())):
     N, a, d = int(input()), list(map(int,input().split())), list(map(int,input().split()))
-    willSurviveDefense = []
+    willSurviveAttack = []
     for current in range(N):
         left = current - 1
         if left == -1: left = N - 1
         right = current + 1
         if right == N: right = 0
         if d[current] > a[left] and d[current] > a[right] and d[current] > a[left] + a[right]:
-            willSurviveDefense.append(d[current])
-    if not willSurviveDefense:
+            willSurviveAttack.append(d[current])
+    if not willSurviveAttack:
         print(-1)
     else:
-        print(max(willSurviveDefense))
+        print(max(willSurviveAttack))
